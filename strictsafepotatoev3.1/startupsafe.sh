5acee5b1c1a0e15ba2718f8166fb70d06e29531f
magus-invictus <73894813+magus-invictus@users.noreply.github.com>
2020-11-03 15:29:18 +0100

    Add files via upload

---
+++ strictsafepotatoev3.1/startupsafe.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+wget "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn/hosts" -O /etc/hosts
+rm -r /etc/resolv.conf
+echo "nameserver 185.228.168.10" >> /etc/resolv.conf
+cat hosts >> /etc/hosts
+chattr +i /etc/resolv.conf
+exit 0
